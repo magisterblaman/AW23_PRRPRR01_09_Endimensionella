e646cd4a22f8106a292e06c3d679d820b41d126c
magisterblaman <hannes.blaman@lbs.se>
2024-10-10 11:30:21 +0200

    Enkla endimensionella

--- AW23_PRRPRR01_09_Endimensionella/Program.cs
+++ AW23_PRRPRR01_09_Endimensionella/Program.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 namespace AW23_PRRPRR01_09_Endimensionella {
 	internal class Program {
 		static void Main(string[] args) {
+			for (int i = 5; i > 11; i /= 2) {
+				Console.WriteLine(i);
+			}
 		}
 	}
 }
